05344b5a0132d02bc0d57c47eb54a88e76f32278
Gavintain <74239444+Gavintain@users.noreply.github.com>
2023-08-23 17:08:44 +0900

    1차 업데이트

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 import os
 
-
 # app inital
 app = Flask(__name__)
 app.static_folder = 'static'
@@ -11,6 +10,22 @@ app.static_folder = 'static'
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/modeltestoverview')
+def model_performance_test():
+    return render_template('modeloverview.html')
+
+@app.route('/modelstatistic')
+def model_statistic():
+    return render_template('model_statistic.html')
+
+@app.route('/taskdata')
+def taskdata():
+    return render_template('taskdata.html')
+
+@app.route('/modeltesting')
+def modeltesting():
+    return render_template('modeltesting.html')
 
 
 # main
